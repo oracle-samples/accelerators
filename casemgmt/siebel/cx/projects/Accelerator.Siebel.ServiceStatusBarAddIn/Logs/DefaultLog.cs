059c5fc7dc37a231d8f87c5cb76107085c8e82d6
Lucy <lliu@Lucy-Mac.local>
2015-11-21 20:09:34 -0800

    fix runtime issue

--- casemgmt/siebel/cx/projects/Accelerator.Siebel.ServiceStatusBarAddIn/Logs/DefaultLog.cs
+++ casemgmt/siebel/cx/projects/Accelerator.Siebel.ServiceStatusBarAddIn/Logs/DefaultLog.cs
@@ -8,7 +8,7 @@
  *  OSvC release: 15.8 (August 2015)
  *  Siebel release: 8.1.1.15
  *  reference: 150520-000047
- *  date: Thu Nov 12 00:55:36 PST 2015
+ *  date: Thu Nov 21 00:55:36 PST 2015
 
  *  revision: rnw-15-11-fixes-release-1
  *  SHA1: $Id: 08d9b348ab8391e7fd626f2f4125749f475c0df7 $
@@ -26,6 +26,10 @@ namespace Accelerator.Siebel.SharedServices.Logs
 {
     internal class DefaultLog : Log
     {
+
+        public DefaultLog(string param1 = null, string param2 = null, string param3 = null)
+        {
+        }
 
         public void ErrorLog(Incident incident = null, RightNowServiceReference.Contact contact = null, string LogMessage = null, string LogNote = null, string source = null, int timeElapsed = 0)
         {
