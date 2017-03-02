e24ea9a926a6565a417de66beefa595f7635a8b1
Scott Harwell <scott@harwell.me>
2017-03-02 17:01:03 -0500

    SRM Accel License Correction

--- social/scripts/custom/srmoauth.php
+++ social/scripts/custom/srmoauth.php
@@ -1,4 +1,22 @@
 <?php
+
+/* *********************************************************************************************
+ *  This file is part of the Oracle Service Cloud Accelerator Reference Integration set published
+ *  by Oracle Service Cloud under the Universal Permissive License (UPL), Version 1.0
+ *  Copyright (c) 2014, 2015, 2016, 2017 Oracle and/or its affiliates. All rights reserved.
+ ***********************************************************************************************
+ *  Accelerator Package: OSvC + SRM Integration Accelerator
+ *  link: http://www.oracle.com/technetwork/indexes/samplecode/accelerator-osvc-2525361.html
+ *  OSvC release: 17.2 (February 2017) 
+ *  reference: 160628-000117
+ *  date: Fri Feb 10 19:47:44 PST 2017
+ 
+ *  revision: rnw-17-2-fixes-release-2
+ *  SHA1: $Id: df36e6c1cf617a66587eb758cb7163047cdba738 $
+ * *********************************************************************************************
+ *  File: srmoauth.php
+ * *********************************************************************************************/
+
 /**
  * This script is the primary script that brokers requests between an SRM add-in and CX or SRM.
  */
--- social/scripts/custom/srmoauthcallback.php
+++ social/scripts/custom/srmoauthcallback.php
@@ -1,5 +1,23 @@
 #!/usr/bin/php
 <?php
+
+/* *********************************************************************************************
+ *  This file is part of the Oracle Service Cloud Accelerator Reference Integration set published
+ *  by Oracle Service Cloud under the Universal Permissive License (UPL), Version 1.0
+ *  Copyright (c) 2014, 2015, 2016, 2017 Oracle and/or its affiliates. All rights reserved.
+ ***********************************************************************************************
+ *  Accelerator Package: OSvC + SRM Integration Accelerator
+ *  link: http://www.oracle.com/technetwork/indexes/samplecode/accelerator-osvc-2525361.html
+ *  OSvC release: 17.2 (February 2017) 
+ *  reference: 160628-000117
+ *  date: Fri Feb 10 19:47:44 PST 2017
+ 
+ *  revision: rnw-17-2-fixes-release-2
+ *  SHA1: $Id: df36e6c1cf617a66587eb758cb7163047cdba738 $
+ * *********************************************************************************************
+ *  File: srmoauthcallback.php
+ * *********************************************************************************************/
+
 if (!isset($_SERVER['RNT_SSL']))
 {
     die("You must submit this request via HTTPS");
